d04bc23532831604536158617aa0c952ecfbe77c
Rasim <eferiums@yandex.ru>
2024-11-02 16:19:19 +0400

    feat: добавлена поддержка CORS

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.js";
-import blogRoutes from './routes/blog.js'
+import blogRoutes from "./routes/blog.js";
 
 dotenv.config();
 
@@ -15,10 +16,19 @@ mongoose
   .then(() => console.log("Подключено"))
   .catch((err) => console.error("Ошибка", err));
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/post", blogRoutes)
+app.use("/api/post", blogRoutes);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен. http://localhost:${PORT}`);
