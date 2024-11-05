0498e563b609a937e61de751d147a00ba7249e7e
Rasim <eferiums@yandex.ru>
2024-11-05 16:12:17 +0400

    fix: исправлена ошибка возврата данных при авторизации

--- controller/UserController.js
+++ controller/UserController.js
@@ -59,7 +59,14 @@ export const login = async (req, res) => {
       expiresIn: "1h",
     });
 
-    res.json({ token });
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     res.status(500).json({
       message: "Не удалось войти",
